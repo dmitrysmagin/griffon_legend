988d58fa301f0d65e44d4ba1e79d8546d1b791db
Dmitry Smagin <exmortis@yandex.ru>
2013-10-10 08:40:49 +0400

    Update license header for griffon.c

--- griffon.c
+++ griffon.c
@@ -1,4 +1,36 @@
 /*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <sys/stat.h>
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+
+#include "griffon.h"
+#include "config.h"
+#include "state.h"
+
+// memo 
+/*
  in game scripts
    2 - find master key
    3 - find crystal
@@ -45,20 +77,6 @@
  20 - armour3
 
 */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <sys/stat.h>
-
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_mixer.h>
-
-#include "griffon.h"
-#include "config.h"
-#include "state.h"
 
 // system
 SDL_Surface *video, *videobuffer, *videobuffer2, *videobuffer3;
